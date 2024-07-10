834c4dd654a0794bba2c0d934529e8bb381b652f
renmindeqinwuyuan <tangwei9705@gmail.com>
2024-07-10 10:18:17 +0800

    自用修改脚本

---
+++ 自用脚本/外语终结者.user.js
@@ -0,0 +1,401 @@
+// ==UserScript==
+// @name         外语终结者
+// @namespace    millle@foxmail.com
+// @version      1.3.98
+// @description  识别非中文字符，如果长度大于5且翻译文本中不含中文，则替换中文翻译
+// @license      MIT
+// @author       人民的勤务员 <millle@foxmail.com>
+// @match        *://*/*
+// @grant        GM_xmlhttpRequest
+// @icon           https://immersive-translate.owenyoung.com/favicon.png
+// @grant        GM_getValue
+ // @grant        GM_addStyle
+// @grant        GM_setValue
+// @grant        GM_registerMenuCommand
+// @connect      translate.googleapis.com
+// @downloadURL https://update.greasyfork.org/scripts/497560/%E5%A4%96%E8%AF%AD%E7%BB%88%E7%BB%93%E8%80%85.user.js
+// @updateURL https://update.greasyfork.org/scripts/497560/%E5%A4%96%E8%AF%AD%E7%BB%88%E7%BB%93%E8%80%85.meta.js
+// ==/UserScript==
+//https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=zh-CN&dj=1&dt=t&dt=rm&q=你好
+GM_addStyle(`
+    /* 高亮覆盖样式 */
+    .highlight-overlay {
+        position: absolute;
+        pointer-events: none;
+        z-index: 9999;
+        border: 2px solid rgba(0, 0, 255, 0.5);
+        background-color: rgba(0, 0, 255, 0.1);
+    }
+
+    /* 信息框样式 */
+    .info-box {
+        position: fixed;
+        top: 10px;
+        left: 10px;
+        z-index: 10000;
+        padding: 10px;
+        background-color: #333; /* 深色模式背景 */
+        color: #fff; /* 深色模式文本颜色 */
+        border: 1px solid #fff; /* 白色边框 */
+        box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
+        font-family: 'Droid Sans Mono', monospace; /* Droid Sans Mono 字体 */
+        font-size: 12px;
+        max-width: 300px;
+    }
+`);
+
+(function() {
+    'use strict';
+let 高亮覆盖 = null;
+    let 信息框 = null;
+      let 鼠标监听启用 = false;
+    let 鼠标移入监听器 = null;
+    let 鼠标点击监听器 = null;
+    // 添加一个对象来跟踪已经翻译过的节点
+    var translatedNodes = {};
+var skipClasses = GM_getValue('skipClasses') || ['prettyprint', 'linenums', 'lang-js'];
+    // 创建一个 MutationObserver 实例
+    var observer = new MutationObserver(function(mutationsList) {
+        // 遍历每一个发生变化的 mutation
+        for(var mutation of mutationsList) {
+            // 检查是否有子节点被添加到文档中
+            //if (mutation.type === 'childList') {
+                // 遍历新增加的节点
+                mutation.addedNodes.forEach(function(addedNode) {
+                    // 如果新增节点是元素节点，可以递归扫描子节点以查找文本内容
+                    if (addedNode.nodeType === Node.ELEMENT_NODE) {
+                      if (!isInsidePreCode(addedNode)) {
+        scanTextNodes(addedNode);
+    }
+                    //  scanTextNodes(addedNode);
+                       // console.log("New element node added:", addedNode.nodeName);
+                    }
+                });
+           // }
+        }
+    });
+function isInsidePreCode(node) {
+    while (node) {
+        if (node.matches && node.matches('pre')) {
+            return true;
+        }
+        node = node.parentElement;
+    }
+    return false;
+}
+    // 配置 MutationObserver 监听的目标节点和观察的属性
+    var config = { childList: true, subtree: true };
+
+    // 开始监听文档的变化
+    observer.observe(document.body, config);
+
+    // 添加或移除翻译功能
+    function toggleTranslation() {
+        var currentSite = window.location.hostname;
+        var enabledSites = GM_getValue('enabledSites') || [];
+        if (enabledSites.includes(currentSite)) {
+            enabledSites = enabledSites.filter(function(site) {
+                return site !== currentSite;
+            });
+            GM_setValue('enabledSites', enabledSites);
+            Toast('已移出翻译！', 3000, 'rgba(0, 128, 0, 0.7)', '#fff');
+            location.reload();
+        } else {
+            enabledSites.push(currentSite);
+            GM_setValue('enabledSites', enabledSites);
+            Toast('已添加翻译！', 3000, 'rgba(0, 128, 0, 0.7)', '#fff');
+            location.reload();
+        }
+    }
+  var skipClasses = ['prettyprint', 'linenums', 'lang-js'];
+
+function shouldSkipElement(element) {
+    // 跳过类名包含 "prettyprint"、"linenums" 和 "lang-js" 的元素
+    if (element.classList.contains('prettyprint') ||
+        element.classList.contains('linenums') ||
+        element.classList.contains('lang-js')) {
+        return true;
+    }
+    // 还可以根据其他需要跳过的条件进行判断
+    return false;
+}
+
+    // 编辑生效站点列表
+    function editSites() {
+        // 检查是否已经存在编辑窗口
+        if (document.getElementById('editorContainer')) {
+            return;
+        }
+
+        var enabledSites = GM_getValue('enabledSites') || [];
+        var siteList = enabledSites.join('\n');
+        var editorWindow = document.createElement('div');
+        editorWindow.innerHTML = `
+            <div id="editorContainer" style="position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); background: white; padding: 20px; border: 1px solid #ccc; border-radius: 5px; z-index: 9999;">
+                <textarea id="siteList" style="width: 300px; height: 200px; margin-bottom: 10px;">${siteList}</textarea><br>
+                <button id="saveButton" style="padding: 5px 10px; background: #007bff; color: #fff; border: none; border-radius: 3px; cursor: pointer;">保存</button>
+                <button id="cancelButton" style="margin-left: 10px; padding: 5px 10px; background: #ccc; color: #333; border: none; border-radius: 3px; cursor: pointer;">取消</button>
+            </div>
+            <div id="overlay" style="position: fixed; top: 0; left: 0; width: 100%; height: 100%; background: rgba(0, 0, 0, 0.5); z-index: 999;"></div>
+        `;
+        document.body.appendChild(editorWindow);
+
+        var saveButton = document.getElementById('saveButton');
+        var cancelButton = document.getElementById('cancelButton');
+        var siteListTextarea = document.getElementById('siteList');
+        var overlay = document.getElementById('overlay');
+
+        saveButton.addEventListener('click', function() {
+            var editedSites = siteListTextarea.value.split('\n').map(function(site) {
+                return site.trim();
+            }).filter(Boolean);
+            GM_setValue('enabledSites', editedSites);
+            Toast('已保存生效站点列表！', 3000, 'rgba(0, 128, 0, 0.7)', '#fff');
+            document.body.removeChild(editorWindow);
+            document.body.removeChild(overlay);
+        });
+
+        cancelButton.addEventListener('click', function() {
+            document.body.removeChild(editorWindow);
+            document.body.removeChild(overlay);
+        });
+    }
+
+    // 注册油猴菜单命令
+    GM_registerMenuCommand(isCurrentSiteEnabled() ? '移出翻译' : '添加翻译', toggleTranslation);
+    GM_registerMenuCommand('编辑生效站点', editSites);
+ GM_registerMenuCommand('开启/关闭高亮和删除功能', toggleMouseListeners);
+
+    // 递归遍历给定节点及其后代（深度优先搜索）
+    function scanTextNodes(node) {
+        if (!node.parentNode || !document.body.contains(node)) {
+            return;
+        }
+
+        switch (node.nodeType) {
+            case Node.ELEMENT_NODE:
+                if (node.tagName.toLowerCase() === 'script' || node.isContentEditable) {
+                    return;
+                }
+                if (shouldSkipElement(node)) {
+                return;
+            }
+                if (shouldTranslateNode(node)) { // 检查是否应该翻译该节点
+                    node.childNodes.forEach(scanTextNodes);
+                }
+                break;
+
+            case Node.TEXT_NODE:
+                var text = node.nodeValue.trim();
+                if (!containsChinese(text) && text.length > 5  && isCurrentSiteEnabled() && shouldTranslateNode(node.parentNode)) {
+                    translateText(node, text);
+                }
+                break;
+        }
+    }
+
+    // 检查当前站点是否在生效站点列表中
+    function isCurrentSiteEnabled() {
+        var currentSite = window.location.hostname;
+        var enabledSites = GM_getValue('enabledSites') || [];
+        return enabledSites.includes(currentSite);
+    }
+
+    // 检查字符串是否含中文或者片假字
+    function containsChinese(str) {
+        // 日文片假字范围：\u3040-\u30FF\uFF66-\uFF9F
+        var katakanaRegex = /[\u3040-\u30FF\uFF66-\uFF9F]/;
+        if (katakanaRegex.test(str)) {
+            return false; // 如果包含日文片假字，则返回 false
+        }
+
+        // 中文字符范围：\u4E00-\u9FA5
+        var chineseRegex = /[\u4E00-\u9FA5]/;
+        return chineseRegex.test(str); // 返回是否包含中文字符
+    }
+
+    // 检查字符串是否含有数字
+    function containsNumbers(str) {
+        return /^\s*[a-zA-Z\u4E00-\u9FA5\s]+\s*$/.test(str);
+    }
+
+    // 检查字符串是否为网址
+    function isURL(str) {
+        return /((https?:\/\/|www\.)[\x21-\x7e]+[\w\/=]|\w([\w._-])+@\w[\w\._-]+\.(com|cn|org|net|info|tv|cc|gov|edu)|(\w[\w._-]+\.(com|cn|org|net|info|tv|cc|gov|edu))(\/[\x21-\x7e]*[\w\/])?|ed2k:\/\/[\x21-\x7e]+\|\/|thunder:\/\/[\x21-\x7e]+=)/gi.test(str);
+    }
+
+    // 检查是否应该翻译该节点
+    function shouldTranslateNode(node) {
+        // 检查是否已经翻译过该节点
+        return !translatedNodes[node.textContent];
+    }
+
+    // 翻译文本
+    function translateText(node, text) {
+        // 检查是否已经翻译过该节点
+        if (translatedNodes[node.textContent]) {
+            return;
+        }
+
+        var api = 'https://translate.googleapis.com/translate_a/single';
+        var params = {
+            client: 'gtx',
+            dt: 't',
+            sl: 'auto',
+            tl: 'zh-CN',
+            q: text
+        };
+
+        GM_xmlhttpRequest({
+            method: 'GET',
+            url: api + buildQueryString(params),
+            onload: function(response) {
+                try {
+                    var data = JSON.parse(response.responseText.replace("'", '\u2019'));
+                    var translatedText = data[0][0][0];
+                    showTranslation(node, text, translatedText);
+                    // 标记该节点已经翻译过
+                    translatedNodes[node.textContent] = true;
+                } catch (error) {
+                    console.error('翻译失败：', error);
+                }
+            },
+            onerror: function(response) {
+                console.error('请求翻译失败：', response.statusText);
+            }
+        });
+    }
+
+    // 构建查询字符串
+    function buildQueryString(params) {
+        return '?' + Object.keys(params).map(function(key) {
+            return encodeURIComponent(key) + '=' + encodeURIComponent(params[key]);
+        }).join('&');
+    }
+
+    // 显示翻译
+    // 显示翻译
+function showTranslation(node, originalText, translatedText) {
+    // 创建 <ruby> 元素和 <rt> 元素
+    var rubyElement = document.createElement('ruby'); // 创建包含音标的元素
+    var rtElement = document.createElement('rt'); // 创建音标元素
+
+    // 设置音标元素的文本内容
+    rtElement.textContent = translatedText;
+    // 设置音标元素的字体大小
+    rtElement.style.fontSize = 'smaller';
+    // 设置音标元素的文本左对齐
+    rtElement.style.textAlign = 'left';
+    rtElement.style.color = 'red'; // 设置音标元素的颜色
+
+    // 将音标元素添加到包含音标的元素中
+    rubyElement.appendChild(rtElement);
+    // 在中文翻译节点之后插入包含音标的元素
+    //node.parentNode.insertBefore(rubyElement, node.nextSibling);
+
+    // 将原始的非中文字符串替换为翻译后的文本
+    node.textContent = translatedText; // 替换原始的非中文字符串
+}
+
+    // 主函数
+    function main() {
+        // 在页面加载时进行一次扫描
+      scanTextNodes(document.body);
+    }
+
+    // 启动脚本
+    main();
+
+    // 弹出提示信息
+    function Toast(msg, duration, backgroundColor, textColor) {
+        duration = isNaN(duration) ? 3000 : duration;
+        backgroundColor = backgroundColor || 'rgba(0, 0, 0, 0.7)';
+        textColor = textColor || 'rgb(255, 255, 255)';
+
+        var m = document.createElement('div');
+        m.innerHTML = msg;
+        m.style.cssText = "max-width:60%;min-width: 150px;padding:0 14px;height: 40px;color: " + textColor + ";line-height: 40px;text-align: center;border-radius: 12px;position: fixed;top: 95%;left: 50%;transform: translate(-50%, -50%);z-index: 2147483647;background: " + backgroundColor + ";font-size: 16px;";
+        document.body.appendChild(m);
+
+        setTimeout(function () {
+            var d = 0.5;
+            m.style.transition = '-webkit-transform ' + d + 's ease-in, opacity ' + d + 's ease-in';
+            m.style.opacity = '0';
+            setTimeout(function () {
+                document.body.removeChild(m)
+            }, d * 1000);
+        }, duration);
+    }
+
+function toggleMouseListeners() {
+        if (!鼠标监听启用) {
+            鼠标移入监听器 = function(event) {
+                var target = event.target;
+                if (target && target.nodeType === Node.ELEMENT_NODE) {
+                    var rect = target.getBoundingClientRect();
+                    if (!高亮覆盖) {
+                        高亮覆盖 = document.createElement('div');
+                        高亮覆盖.className = 'highlight-overlay';
+                        document.body.appendChild(高亮覆盖);
+                    }
+                    高亮覆盖.style.top = `${rect.top + window.scrollY}px`;
+                    高亮覆盖.style.left = `${rect.left + window.scrollX}px`;
+                    高亮覆盖.style.width = `${rect.width}px`;
+                    高亮覆盖.style.height = `${rect.height}px`;
+                }
+            };
+
+            鼠标点击监听器 = function(event) {
+                var target = event.target;
+                if (target && target.nodeType === Node.ELEMENT_NODE) {
+                    if (!信息框) {
+                        信息框 = document.createElement('div');
+                        信息框.className = 'info-box';
+                        document.body.appendChild(信息框);
+                    }
+                    信息框.innerHTML = `
+                        节点名称: ${target.nodeName}<br>
+                        类名: ${target.className}<br>
+                        ID: ${target.id}<br>
+                        内部文本: ${target.innerText.slice(0, 100)}
+                    `;
+                    信息框.style.display = 'block';
+
+                    // 删除目标元素的父节点
+                 ///   target.parentNode.removeChild(target);
+
+
+
+                skipElements.push(target.className);
+                console.log('已将节点ID添加到跳过名单：', target.className);
+
+
+            GM_setValue('skipClasses', skipElements);
+
+                }
+            };
+
+            document.addEventListener('mouseover', 鼠标移入监听器);
+            document.addEventListener('click', 鼠标点击监听器);
+
+            鼠标监听启用 = true;
+            Toast('已开启高亮和删除功能', 3000, 'rgba(0, 128, 0, 0.7)', '#fff');
+        } else {
+            document.removeEventListener('mouseover', 鼠标移入监听器);
+            document.removeEventListener('click', 鼠标点击监听器);
+
+            if (高亮覆盖) {
+                document.body.removeChild(高亮覆盖);
+                高亮覆盖 = null;
+            }
+
+            if (信息框) {
+                document.body.removeChild(信息框);
+                信息框 = null;
+            }
+
+            鼠标监听启用 = false;
+            Toast('已关闭高亮和删除功能', 3000, 'rgba(0, 128, 0, 0.7)', '#fff');
+        }
+    }
+})();
---
+++ 自用脚本/置頂和置底按鈕.user.js
@@ -0,0 +1,163 @@
+// ==UserScript==
+// @name        置頂和置底按鈕
+// @version     1.1
+// @description 在所有頁面生成兩個按鈕，一個用於順滑回到頂部，一個用於持續滾動到底部，再次點擊取消滾動到底部
+// @run-at      document-end
+// @match       *://*/*
+// @license     MIT
+// @namespace   https://greasyfork.org/zh-CN/users/954189
+// @downloadURL https://update.greasyfork.org/scripts/498420/%E7%BD%AE%E9%A0%82%E5%92%8C%E7%BD%AE%E5%BA%95%E6%8C%89%E9%88%95.user.js
+// @updateURL https://update.greasyfork.org/scripts/498420/%E7%BD%AE%E9%A0%82%E5%92%8C%E7%BD%AE%E5%BA%95%E6%8C%89%E9%88%95.meta.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+let scrollTimeout;
+    var style = document.createElement('style');
+    style.innerHTML = `
+:root {
+    --button-size: 29.4px; /* 按钮的大小 */
+    --button-margin: 1px; /* 按钮之间的间距 */
+}
+
+.GO_TO_TOP_button, .GO_TO_BOTTOM_button {
+    width: var(--button-size);  /* 按钮大小 */
+    height: var(--button-size);  /* 按钮大小 */
+    border-radius: 5.6px;  /* 縮小30% */
+    box-shadow: 0 3px 6px rgb(0 0 0 / 16%), 0 1px 2px rgb(0 0 0 / 23%);
+    position: fixed;
+    right: 14px;  /* 缩小30% */
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    z-index: 99999999;
+    background-color: white;
+    opacity: 0.8;
+    transition: opacity 0.2s ease-in-out;
+    cursor: pointer;
+}
+
+
+
+    .GO_TO_TOP_button svg, .GO_TO_BOTTOM_button svg {
+        left: 16.8px;  /* 縮小30% */
+        height: 16.8px;  /* 縮小30% */
+        margin: 0;
+    }
+    .GO_TO_TOP_button {
+        bottom: 49px;  /* 縮小30% */
+    }
+    .GO_TO_BOTTOM_button {
+        bottom: 14px;  /* 縮小30% */
+    }
+    `;
+    document.head.appendChild(style);
+
+    const goToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    }
+
+    let bottomInterval;
+    const startScrollingToBottom = () => {
+        bottomInterval = setInterval(() => {
+            window.scrollTo({
+                top: document.body.scrollHeight,
+                behavior: 'smooth'
+            });
+        }, 1000);
+
+let checkTimeout = setTimeout(() => {
+        if (window.innerHeight + window.scrollY >= document.body.scrollHeight) {
+           if (buttonBottom.style.backgroundColor === 'green') {
+                buttonBottom.click();
+              //  alert("已处于最底端");
+            }
+        }
+    }, 3000);
+
+    window.addEventListener('scroll', () => {
+        if (window.innerHeight + window.scrollY < document.body.scrollHeight) {
+            clearTimeout(checkTimeout);
+            checkTimeout = setTimeout(() => {
+                if (buttonBottom.style.backgroundColor === 'green') {
+                        buttonBottom.click();
+                  //      alert("自动滚动到底部已停止。");
+                    }
+            }, 3000);
+        }
+    });
+  buttonBottom.style.opacity = '0.8';
+    buttonBottom.style.display = 'flex';
+}
+    const stopScrollingToBottom = () => {
+        clearInterval(bottomInterval);
+    }
+
+    const buttonTop = document.createElement('div');
+    buttonTop.className = 'GO_TO_TOP_button';
+    buttonTop.innerHTML = `
+    <svg viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg">
+        <path d="M825.568 555.328l-287.392-289.28C531.808 259.648 523.488 256.576 515.2 256.64 514.08 256.544 513.12 256 512 256c-4.672 0-9.024 1.088-13.024 2.88-4.032 1.536-7.872 3.872-11.136 7.136l-259.328 258.88c-12.512 12.48-12.544 32.736-0.032 45.248 6.24 6.272 14.432 9.408 22.656 9.408 8.192 0 16.352-3.136 22.624-9.344L480 364.288V928c0 17.696 14.336 32 32 32s32-14.304 32-32V362.72l236.192 237.728c6.24 6.272 14.496 9.44 22.688 9.44s16.32-3.104 22.56-9.312C838.016 588.128 838.048 567.84 825.568 555.328z"/>
+        <path d="M864 192H160C142.336 192 128 177.664 128 160s14.336-32 32-32h704c17.696 0 32 14.336 32 32S881.696 192 864 192z"/>
+    </svg>`;
+
+    const buttonBottom = document.createElement('div');
+    buttonBottom.className = 'GO_TO_BOTTOM_button';
+    buttonBottom.innerHTML = `
+    <svg viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg">
+        <path d="M198.4 468.352l287.392 289.28c6.368 6.4 14.688 9.472 22.976 9.408 1.12 0.096 2.08 0.64 3.2 0.64 4.672 0 9.024-1.088 13.024-2.88 4.032-1.536 7.872-3.872 11.136-7.136l259.328-258.88c12.512-12.48 12.544-32.736 0.032-45.248-6.24-6.272-14.432-9.408-22.656-9.408-8.192 0-16.352 3.136-22.624 9.344L544 659.712V96c0-17.696-14.336-32-32-32s-32 14.304-32 32v565.28L243.808 423.552c-6.24-6.272-14.496-9.44-22.688-9.44s-16.32 3.104-22.56 9.312c-12.48 12.512-12.512 32.8-0.032 45.312z"/>
+        <path d="M160 832h704c17.664 0 32 14.336 32 32s-14.336 32-32 32H160c-17.664 0-32-14.336-32-32s14.336-32 32-32z"/>
+    </svg>`;
+
+    let isScrollingToBottom = false;
+
+    buttonTop.addEventListener('click', goToTop);
+
+    buttonBottom.addEventListener('click', () => {
+        if (isScrollingToBottom) {
+            stopScrollingToBottom();
+            buttonBottom.style.transform = 'scale(1)';
+             buttonBottom.style.backgroundColor = 'white'; // 改回原来的背景色
+        } else {
+            startScrollingToBottom();
+            buttonBottom.style.transform = 'scale(1.1)';
+            buttonBottom.style.backgroundColor = 'green'; // 改为绿色
+        }
+        isScrollingToBottom = !isScrollingToBottom;
+    });
+
+    buttonTop.style.display = 'none';
+    buttonBottom.style.display = 'none';
+    document.body.appendChild(buttonTop);
+    document.body.appendChild(buttonBottom);
+
+    let timer;
+    window.addEventListener('scroll', () => {
+        if (window.pageYOffset > 50) {
+            buttonTop.style.opacity = '0.8';
+            buttonTop.style.display = 'flex';
+            buttonBottom.style.opacity = '0.8';
+            buttonBottom.style.display = 'flex';
+        } else if (window.pageYOffset === 0) {
+            buttonTop.style.opacity = '0';
+            buttonBottom.style.opacity = '0';
+            setTimeout(() => {
+                buttonTop.style.display = 'none';
+                buttonBottom.style.display = 'none';
+            }, 200);
+        }
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            buttonTop.style.opacity = '0';
+            buttonBottom.style.opacity = '0';
+            setTimeout(() => {
+                buttonTop.style.display = 'none';
+                buttonBottom.style.display = 'none';
+            }, 200);
+        }, 1800);
+
+    });
+})();
