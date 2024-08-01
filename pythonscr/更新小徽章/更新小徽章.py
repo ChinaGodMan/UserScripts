2c0abea93d11b9b21fea970593b0f01f13becc1b
人民的勤务员 <96548841+ChinaGodMan@users.noreply.github.com>
2024-08-01 20:17:01 +0800

    更新

--- pythonscr/更新小徽章/更新小徽章.py
+++ pythonscr/更新小徽章/更新小徽章.py
@@ -2,7 +2,8 @@ import os
 
 # 定义分隔符和新内容文件的路径
 SEPARATOR = 'https://media.chatgptautorefresh.com/images/separators/gradient-aqua.png?latest">'
-NEW_CONTENT_PATH = os.path.join('pythonscr', 'new')
+script_dir = os.path.dirname(os.path.abspath(__file__))
+NEW_CONTENT_PATH = os.path.join(script_dir, 'NewMD')
 
 def get_new_content():
     with open(NEW_CONTENT_PATH, 'r', encoding='utf-8') as file:
