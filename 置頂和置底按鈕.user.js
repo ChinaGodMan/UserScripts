88504dd34b282f82a6d5159d1c4177fb0df2f2df
人民的勤务员 <96548841+ChinaGodMan@users.noreply.github.com>
2024-08-26 01:34:05 +0800

    - 2024/8/26 1:28.
      >**Ver: 1.2.0.48** <br> [#257324](https://greasyfork.org/zh-CN/scripts/500255/discussions/257324)<br>
      *请求添加绑定热键*

--- 置頂和置底按鈕.user.js
+++ 置頂和置底按鈕.user.js
@@ -21,26 +21,80 @@
 // @description:es Genera dos botones en todas las páginas, uno para desplazarse suavemente hacia arriba y otro para desplazarse continuamente hacia abajo. Haz clic de nuevo para detener el desplazamiento hacia abajo.
 // @name:pt        Botões de rolar para cima e para baixo
 // @description:pt Gere dois botões em todas as páginas, um para rolar suavemente para cima e outro para rolar continuamente para baixo. Clique novamente para parar de rolar para baixo.
-// @version 1.2.0.47
+// @version 1.2.0.48
 // @run-at      document-end
 // @match       *://*/*
 // @license     MIT
+// @grant        GM_registerMenuCommand
+// @grant        GM_setValue
+// @grant        GM_getValue
 // @namespace   https://greasyfork.org/zh-CN/users/1169082
 // @icon          https://github.com/ChinaGodMan/UserScripts/raw/main/docs/icon/Scripts%20Icons/icons8-up-96.png
 // @supportURL              https://github.com/ChinaGodMan/UserScripts/issues
 // @homepageURL   https://github.com/ChinaGodMan/UserScripts
 // ==/UserScript==
-
 (function () {
     'use strict'
-    let scrollTimeout
+    let isScrollingToBottom = false
+    let buttonTopKey = GM_getValue("buttonTopKey", "")
+    let buttonBottomKey = GM_getValue("buttonBottomKey", "")
     var style = document.createElement('style')
+    GM_registerMenuCommand("SetHotkey", showHotkeyModal)
+    function showHotkeyModal() {
+        let modalHtml = `
+        <div id="hotkeyModal" style="position: fixed; z-index: 10000; background: white; padding: 20px; border-radius: 8px; box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5); top: 20%; left: 50%; transform: translate(-50%, -20%); max-width: 300px;">
+            <h2 style="margin-top: 0;">ButtonKey</h2>
+            <div style="margin-bottom: 10px;">
+                <label for="hotkeyInput1" style="display: block; margin-bottom: 5px;">buttonTopKey:</label>
+                <input id="hotkeyInput1" style="width: 100%; padding: 8px; border: 1px solid #ccc; border-radius: 4px;" placeholder="${buttonTopKey}" />
+            </div>
+            <div style="margin-bottom: 20px;">
+                <label for="hotkeyInput2" style="display: block; margin-bottom: 5px;">buttonBottomKey:</label>
+                <input id="hotkeyInput2" style="width: 100%; padding: 8px; border: 1px solid #ccc; border-radius: 4px;" placeholder="${buttonBottomKey}" />
+            </div>
+            <div style="text-align: right;">
+                <button id="saveHotkeys" style="background-color: #4CAF50; color: white; padding: 10px 20px; border: none; border-radius: 4px; cursor: pointer;">Save</button>
+                <button id="closeModal" style="background-color: #f44336; color: white; padding: 10px 20px; border: none; border-radius: 4px; cursor: pointer; margin-left: 10px;">Close</button>
+            </div>
+        </div>`
+        let modalDiv = document.createElement('div')
+        modalDiv.innerHTML = modalHtml
+        document.body.appendChild(modalDiv)
+        document.getElementById("hotkeyInput1").addEventListener("keydown", function (event) {
+            event.preventDefault()
+            buttonTopKey = event.key
+            this.value = buttonTopKey
+        })
+        document.getElementById("hotkeyInput2").addEventListener("keydown", function (event) {
+            event.preventDefault()
+            buttonBottomKey = event.key
+            this.value = buttonBottomKey
+        })
+        document.getElementById("saveHotkeys").addEventListener("click", function () {
+            GM_setValue("buttonTopKey", buttonTopKey)
+            GM_setValue("buttonBottomKey", buttonBottomKey)
+            closeModal()
+        })
+        document.getElementById("closeModal").addEventListener("click", closeModal)
+    }
+    function closeModal() {
+        let modal = document.getElementById("hotkeyModal")
+        if (modal) {
+            modal.remove()
+        }
+    }
+    document.addEventListener("keydown", function (event) {
+        if (event.key === GM_getValue("buttonTopKey")) {
+            goToTop()
+        } else if (event.key === GM_getValue("buttonBottomKey")) {
+            isScrollingToBottom = toggleScrolling(buttonBottom, isScrollingToBottom)
+        }
+    })
     style.innerHTML = `
 :root {
     --button-size: 29.4px; /* 按钮的大小 */
     --button-margin: 1px; /* 按钮之间的间距 */
 }
-
 .GO_TO_TOP_button, .GO_TO_BOTTOM_button {
     width: var(--button-size);  /* 按钮大小 */
     height: var(--button-size);  /* 按钮大小 */
@@ -57,9 +111,6 @@
     transition: opacity 0.2s ease-in-out;
     cursor: pointer;
 }
-
-
-
     .GO_TO_TOP_button svg, .GO_TO_BOTTOM_button svg {
         left: 16.8px;  /* 縮小30% */
         height: 16.8px;  /* 縮小30% */
@@ -73,14 +124,12 @@
     }
     `
     document.head.appendChild(style)
-
     const goToTop = () => {
         window.scrollTo({
             top: 0,
             behavior: 'smooth'
         })
     }
-
     let bottomInterval
     const startScrollingToBottom = () => {
         bottomInterval = setInterval(() => {
@@ -89,7 +138,6 @@
                 behavior: 'smooth'
             })
         }, 1000)
-
         let checkTimeout = setTimeout(() => {
             if (window.innerHeight + window.scrollY >= document.body.scrollHeight) {
                 if (buttonBottom.style.backgroundColor === 'green') {
@@ -98,7 +146,6 @@
                 }
             }
         }, 3000)
-
         window.addEventListener('scroll', () => {
             if (window.innerHeight + window.scrollY < document.body.scrollHeight) {
                 clearTimeout(checkTimeout)
@@ -116,7 +163,6 @@
     const stopScrollingToBottom = () => {
         clearInterval(bottomInterval)
     }
-
     const buttonTop = document.createElement('div')
     buttonTop.className = 'GO_TO_TOP_button'
     buttonTop.innerHTML = `
@@ -124,7 +170,6 @@
         <path d="M825.568 555.328l-287.392-289.28C531.808 259.648 523.488 256.576 515.2 256.64 514.08 256.544 513.12 256 512 256c-4.672 0-9.024 1.088-13.024 2.88-4.032 1.536-7.872 3.872-11.136 7.136l-259.328 258.88c-12.512 12.48-12.544 32.736-0.032 45.248 6.24 6.272 14.432 9.408 22.656 9.408 8.192 0 16.352-3.136 22.624-9.344L480 364.288V928c0 17.696 14.336 32 32 32s32-14.304 32-32V362.72l236.192 237.728c6.24 6.272 14.496 9.44 22.688 9.44s16.32-3.104 22.56-9.312C838.016 588.128 838.048 567.84 825.568 555.328z"/>
         <path d="M864 192H160C142.336 192 128 177.664 128 160s14.336-32 32-32h704c17.696 0 32 14.336 32 32S881.696 192 864 192z"/>
     </svg>`
-
     const buttonBottom = document.createElement('div')
     buttonBottom.className = 'GO_TO_BOTTOM_button'
     buttonBottom.innerHTML = `
@@ -132,12 +177,11 @@
         <path d="M198.4 468.352l287.392 289.28c6.368 6.4 14.688 9.472 22.976 9.408 1.12 0.096 2.08 0.64 3.2 0.64 4.672 0 9.024-1.088 13.024-2.88 4.032-1.536 7.872-3.872 11.136-7.136l259.328-258.88c12.512-12.48 12.544-32.736 0.032-45.248-6.24-6.272-14.432-9.408-22.656-9.408-8.192 0-16.352 3.136-22.624 9.344L544 659.712V96c0-17.696-14.336-32-32-32s-32 14.304-32 32v565.28L243.808 423.552c-6.24-6.272-14.496-9.44-22.688-9.44s-16.32 3.104-22.56 9.312c-12.48 12.512-12.512 32.8-0.032 45.312z"/>
         <path d="M160 832h704c17.664 0 32 14.336 32 32s-14.336 32-32 32H160c-17.664 0-32-14.336-32-32s14.336-32 32-32z"/>
     </svg>`
-
-    let isScrollingToBottom = false
-
     buttonTop.addEventListener('click', goToTop)
-
     buttonBottom.addEventListener('click', () => {
+        isScrollingToBottom = toggleScrolling(buttonBottom, isScrollingToBottom)
+    })
+    function toggleScrolling(buttonBottom, isScrollingToBottom) {
         if (isScrollingToBottom) {
             stopScrollingToBottom()
             buttonBottom.style.transform = 'scale(1)'
@@ -147,14 +191,12 @@
             buttonBottom.style.transform = 'scale(1.1)'
             buttonBottom.style.backgroundColor = 'green' // 改为绿色
         }
-        isScrollingToBottom = !isScrollingToBottom
-    })
-
+        return !isScrollingToBottom
+    }
     buttonTop.style.display = 'none'
     buttonBottom.style.display = 'none'
     document.body.appendChild(buttonTop)
     document.body.appendChild(buttonBottom)
-
     let timer
     window.addEventListener('scroll', () => {
         if (window.pageYOffset > 50) {
@@ -179,6 +221,5 @@
                 buttonBottom.style.display = 'none'
             }, 200)
         }, 1800)
-
     })
 })()
