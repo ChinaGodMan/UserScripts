322733c345bf67ca4f82af798bb81f7338928fca
人民的勤务员 <96548841+ChinaGodMan@users.noreply.github.com>
2024-09-04 04:58:57 +0800

    更新

--- Script details/greasyfork-discussion-watcher/greasyfork-discussion-watcher.user.js
+++ Script details/greasyfork-discussion-watcher/greasyfork-discussion-watcher.user.js
@@ -41,7 +41,7 @@
 (function () {
     'use strict'
     const config = {
-        isInstalled: GM_getValue('Installed', true),//第一次不加载~,
+        isInstalled: GM_getValue('Installed', false),//第一次不加载~,
         lastUpdated: GM_getValue('lastUpdated', 0),//上次更新时间
         delay: GM_getValue('delay', "30m"), // 格式如下: 1h1m1s, 1h1s, 1m, 1s, 1m1s
         userId: null,//当前登录id
