e41404e04cf406c37a3e971a104fbd5b2a05261b
人民的勤务员 <96548841+ChinaGodMan@users.noreply.github.com>
2024-10-04 23:34:11 +0000

    新增: `仓库内快速跳转其他仓库`

--- github-repo-size-view.user.js
+++ github-repo-size-view.user.js
@@ -78,7 +78,7 @@
 // @description:fr-CA exister github Ajouter la taille du référentiel à côté du nom du référentiel sur les pages de recherche et du référentiel
 // @namespace         https://github.com/ChinaGodMan/UserScripts
 // @description       Adds the repo size next to the repo name on github search and repo pages
-// @version           0.1.3.2
+// @version           0.1.3.3
 // @author            mshll & 人民的勤务员 <toniaiwanowskiskr47@gmail.com>
 // @match             https://github.com/*
 // @grant             none
@@ -117,6 +117,9 @@ const translations = {
         renderCaution: "Caution: repo has not received an update in 6+ months",
         confirm: "You have not entered a Token, confirm to clear the GitHub Token?",
         timediff: "Last commit was: {years} years, {months} months, {days} days ago ",
+        view: "View[",
+        allRepos: "]All repositories",
+        newTab: "Open in a new tab when quickly viewing repositories",
     },
     "zh-CN,zh,zh-SG": {
         save: "保存",
@@ -131,6 +134,10 @@ const translations = {
         renderCaution: "注意：该仓库在 6 个月以上未更新",
         confirm: "你没有输入Token,确认清空GitHub Token?",
         timediff: "最后一次提交距现在：{years}年{months}个月{days}天 ",
+        view: "查看[",
+        allRepos: "]所有仓库",
+        newTab: "快速查看仓库时新窗口打开",
+
     },
     "zh-TW,zh-HK,zh-MO": {
         save: "保存",
@@ -215,6 +222,7 @@ const translate = new Proxy(
 //*Note: to be able to see the size of your private repos, you need to select the `repo` scope when generating the token
 let TOKEN = GM_getValue("githubToken", "")
 let WARNING = GM_getValue("warn", true)
+let openInNewTab = GM_getValue("openInNewTab", false)
 GM_addStyle(`
     .modal-overlay{position:fixed;top:0;left:0;width:100%;height:100%;background:rgba(0,0,0,0.5);display:flex;justify-content:center;align-items:center;z-index:1000;}
     .modal-content{background:white;padding:20px;border-radius:8px;width:400px;box-shadow:0 4px 15px rgba(0,0,0,0.2);position:relative;}
@@ -237,7 +245,8 @@ function createModal() {
                     </a>
                 </p>
                 <input type="text" id="github-token-input" placeholder="${translate.githubtokeninput}">
-                <label><input type="checkbox" id="warn">${translate.warncheckbox}</label>
+                <label><input type="checkbox" id="warn">${translate.warncheckbox}</label><br>
+                <label><input type="checkbox" id="openInNewTab">${translate.newTab}</label>
                 <br>
                 <hr>
                 <button id="save-token">${translate.save}</button>
@@ -251,11 +260,15 @@ function createModal() {
     const input = document.getElementById("github-token-input")
     const warn = document.getElementById("warn")
     warn.checked = GM_getValue("warn", true)
+    const newTab = document.getElementById("openInNewTab")
+    newTab.checked = GM_getValue("openInNewTab", false)
     input.value = GM_getValue("githubToken", "")
     document.getElementById("save-token").addEventListener("click", () => {
         const token = input.value.trim()
         GM_setValue("warn", warn.checked)
         WARNING = warn.checked
+        GM_setValue("openInNewTab", newTab.checked)
+        openInNewTab = newTab.checked
         if (token) {
             GM_setValue("githubToken", token)
             modalContainer.remove()
@@ -348,6 +361,24 @@ const addSizeToRepos = () => {
         // Get parent element to append the size to
         let parent = elem.parentElement
         if (pageType === "repo") {
+            const reposApi = isLoggedInUser(jsn.owner.avatar_url) ? 'https://api.github.com/user/repos' : jsn.owner.repos_url
+            getUserRepos(reposApi, headers)
+                .then(data => {
+                    const reposArray = data.map(repo => ({
+                        name: repo.name,
+                        private: repo.private,
+                        html_url: repo.html_url,
+                        fork: repo.fork,
+                        description: repo.description,
+                        stargazers_count: repo.stargazers_count,
+                        owner: repo.owner.login
+                    }))
+
+                    insertReposList(reposArray)
+
+
+                })
+                .catch(error => console.error('Error fetching data:', error))
             parent = elem.parentElement.parentElement
         }
         // Create the size container
@@ -496,7 +527,6 @@ function renderWarning(timediff) {
     banner.appendChild(smallTag)
     displayMessage(banner)
 }
-
 function renderCaution(timediff) {
     const banner = document.createElement("div")
     banner.id = "zh-banner-warning"
@@ -526,10 +556,8 @@ function renderCaution(timediff) {
     border-top-left-radius: 5px;
   `
     )
-
     smallTag.textContent = timediff
     banner.appendChild(smallTag)
-
     displayMessage(banner)
 }
 function checkCommitDate(datetimeString) {
@@ -558,7 +586,6 @@ function checkCommitDate(datetimeString) {
     result = result.replace('{years}', finalYears > 0 ? finalYears : '')
     result = result.replace('{months}', adjustedMonths)
     result = result.replace('{days}', adjustedDays)
-    console.log(result.trim())
     const daysSinceLastCommit = (Date.now() - date.getTime()) / 1000 / 60 / 60 / 24
     if (daysSinceLastCommit > 365) {
         renderWarning(result)
@@ -566,5 +593,125 @@ function checkCommitDate(datetimeString) {
         renderCaution(result)
     } else {
         /* noop */
+    }
+}
+function insertReposList(links) {
+
+    const gitHubStyle = `
+#view-user-repos {
+  order: 10;
+}
+#view-user-repos .dropdown-menu {
+  min-width: 170px;
+  width: auto;
+}
+#view-user-repos .dropdown-menu .dropdown-item .d-inline-flex {
+  vertical-align:sub;
+}`
+    if (!document.head.querySelector('style[data-id="view-user-repos-css"]')) {
+        const globalStyle = document.createElement('style')
+        globalStyle.dataset.id = 'view-user-repos-css'
+        globalStyle.innerHTML = gitHubStyle
+        document.head.appendChild(globalStyle)
+    }
+    const existingButton = document.querySelector('button.types__StyledButton-sc-ws60qy-0.bmlmSe')
+    if (existingButton) {
+        const sortedLinks = links.sort((a, b) => {//!SECTION 排序
+            // 首先比较 fork
+            if (b.fork > 0 && a.fork <= 0) {
+                return -1 // a 在前
+            } else if (a.fork > 0 && b.fork <= 0) {
+                return 1 // b 在前
+            } else {
+                // 如果 fork 相同，比较 private
+                if (b.private === a.private) {
+                    // 如果 private 相同，进一步判断 private 为 false 的情况
+                    if (!a.private && !b.private) {
+                        return b.stargazers_count - a.stargazers_count // stargazers_count 大的在前
+                    }
+                    return 0 // private 相同且不是 false 的情况下不排序
+                }
+                return (b.private ? -1 : 1) // private 为 true 的在前
+            }
+        })
+        function getIconPath(link) {
+            var fillColor = null
+            if (link.private) fillColor = "green"
+            if (!link.private && !link.fork) fillColor = "red"
+            var svg = `<path fill="${fillColor}" fill-rule="evenodd" d="M1 3.5c0-.626.292-1.165.7-1.59.406-.422.956-.767 1.579-1.041C4.525.32 6.195 0 8 0c1.805 0 3.475.32 4.722.869.622.274 1.172.62 1.578 1.04.408.426.7.965.7 1.591v9c0 .626-.292 1.165-.7 1.59-.406.422-.956.767-1.579 1.041C11.476 15.68 9.806 16 8 16c-1.805 0-3.475-.32-4.721-.869-.623-.274-1.173-.62-1.579-1.04-.408-.426-.7-.965-.7-1.591Zm1.5 0c0 .133.058.318.282.551.227.237.591.483 1.101.707C4.898 5.205 6.353 5.5 8 5.5c1.646 0 3.101-.295 4.118-.742.508-.224.873-.471 1.1-.708.224-.232.282-.417.282-.55 0-.133-.058-.318-.282-.551-.227-.237-.591-.483-1.101-.707C11.102 1.795 9.647 1.5 8 1.5c-1.646 0-3.101.295-4.118.742-.508.224-.873.471-1.1.708-.224.232-.282.417-.282.55Zm0 4.5c0 .133.058.318.282.551.227.237.591.483 1.101.707C4.898 9.705 6.353 10 8 10c1.646 0 3.101-.295 4.118-.742.508-.224.873-.471 1.1-.708.224-.232.282-.417.282-.55V5.724c-.241.15-.503.286-.778.407C11.475 6.68 9.805 7 8 7c-1.805 0-3.475-.32-4.721-.869a6.15 6.15 0 0 1-.779-.407Zm0 2.225V12.5c0 .133.058.318.282.55.227.237.592.484 1.1.708 1.016.447 2.471.742 4.118.742 1.647 0 3.102-.295 4.117-.742.51-.224.874-.47 1.101-.707.224-.233.282-.418.282-.551v-2.275c-.241.15-.503.285-.778.406-1.247.549-2.917.869-4.722.869-1.805 0-3.475-.32-4.721-.869a6.327 6.327 0 0 1-.779-.406Z"></path>`
+            if (fillColor) return svg
+            if (link.fork) return `<path d="M5 5.372v.878c0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75v-.878a2.25 2.25 0 1 1 1.5 0v.878a2.25 2.25 0 0 1-2.25 2.25h-1.5v2.128a2.251 2.251 0 1 1-1.5 0V8.5h-1.5A2.25 2.25 0 0 1 3.5 6.25v-.878a2.25 2.25 0 1 1 1.5 0ZM5 3.25a.75.75 0 1 0-1.5 0 .75.75 0 0 0 1.5 0Zm6.75.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm-3 8.75a.75.75 0 1 0-1.5 0 .75.75 0 0 0 1.5 0Z"></path>`
+        }
+
+        let privateClassAdded = false
+        let forkClassAdded = false
+        const listItems = sortedLinks.map(link => {
+            let liClass = ""
+            if (link.private && !privateClassAdded) {
+                liClass += "d-none d-md-block border-top"
+                privateClassAdded = true
+            }
+            if (link.fork && !forkClassAdded) {
+                liClass += "d-none d-md-block border-top"
+                forkClassAdded = true
+            }
+            return `
+        <li class="${liClass}">
+            <a href="${link.html_url}" class="dropdown-item" ${(openInNewTab) ? 'target="_blank"' : ''} rel="noopener noreferrer">
+                <span class="d-inline-flex mr-2">
+                    <svg width="16" height="16" viewBox="0 0 16 16">
+                        ${getIconPath(link)}
+                    </svg>
+                </span>
+                ${link.name}
+            </a>
+        </li>
+    `
+        }).join('')
+
+        const detailsHTML = `
+<details id="view-user-repos" class="details-overlay details-reset position-relative d-flex">
+    <summary role="button" type="button" class="btn text-center">
+        <span class="d-none d-xl-flex flex-items-center">
+             ${translate.view}<mark>${links[0].owner}</mark>${translate.allRepos}
+            <span class="dropdown-caret ml-2"></span>
+        </span>
+        <span class="d-inline-block d-xl-none">
+            ${links[0].owner} 
+            <span class="dropdown-caret d-none d-sm-inline-block d-md-none d-lg-inline-block"></span>
+        </span>
+    </summary>
+    <div>
+        <ul class="dropdown-menu dropdown-menu-sw">
+            ${listItems}
+        </ul>
+    </div>
+</details>`
+        existingButton.insertAdjacentHTML('beforebegin', detailsHTML)
+    } else {
+
+    }
+}
+function isLoggedInUser(avatar_url) {
+    const imgElement = document.querySelector(".AppHeader-user button span span img")
+    if (imgElement) {
+        const imgSrc = imgElement.src
+        return imgSrc === avatar_url
+    } else {
+        return false
+    }
+}
+async function getUserRepos(href, header = {}) {
+    try {
+        const response = await fetch(`${href}`, {
+            headers: header,
+        })
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`)
+        }
+        return await response.json()
+    } catch (error) {
+        console.error('Fetch error:', error)
+        throw error
     }
 }
