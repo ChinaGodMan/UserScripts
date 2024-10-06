0b1572cd0a34ef7f2ab2089de28da389e2906d49
人民的勤务员 <96548841+ChinaGodMan@users.noreply.github.com>
2024-10-07 07:21:22 +0800

    更新

--- pythonscr/copy_js_files.py
+++ pythonscr/copy_js_files.py
@@ -1,23 +1,26 @@
 import os
 import shutil
 import subprocess
-
-# 定义目标目录
+import json
 target_dir = '.'
-
-# 获取变更的文件列表
-result = subprocess.run(['git', 'diff', '--name-only', 'HEAD^', 'HEAD'], capture_output=True, text=True)
+result = subprocess.run(['git', 'diff', '--name-only',
+                        'HEAD^', 'HEAD'], capture_output=True, text=True)
 changed_files = result.stdout.splitlines()
-
-# 遍历变更的文件列表
+with open('docs/ScriptsPath.json', 'r', encoding='utf-8') as json_file:
+    scripts_data = json.load(json_file)
 for file in changed_files:
     if file.endswith('.js'):
-        # 检查文件是否存在于当前目录
         file_path = os.path.join(os.getcwd(), file)
         if os.path.exists(file_path):
-            target_path = os.path.join(target_dir, os.path.basename(file))
-            subprocess.run(['node', 'utils/single-format-monkey-meta.js', file_path], check=True)
-            # 复制文件
+            file_name = os.path.basename(file)
+            found = any(script['path'] ==
+                        file_name for script in scripts_data['scripts'])
+            if not found:
+                print(f'文件 {file_name} 在 JSON 中找不到，跳过操作。')
+                continue
+            target_path = os.path.join(target_dir, file_name)
+            subprocess.run(
+                ['node', 'utils/single-format-monkey-meta.js', file_path], check=True)
             shutil.copy(file_path, target_path)
             print(f'复制 {file_path} 到 {target_path}')
         else:
