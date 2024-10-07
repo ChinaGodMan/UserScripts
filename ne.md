b390a611c6876f20cd735fc24e5e7feb2bdc1623
人民的勤务员 <96548841+ChinaGodMan@users.noreply.github.com>
2024-10-08 03:43:23 +0800

    有的脚本无更新日志文件，检查更新时间失败，使用`content_snippet.py` 检查历史是否相同使用`process_file`直接写入得了.

---
+++ ne.md
@@ -0,0 +1,11 @@
+
+<center><div align="center">
+    <h1>网页翻译——自动翻译为中文</h1>
+    <p>「 给每个非中文的网页右下角（可以调整到左下角）添加一个 google 翻译图标，该版本为中文翻译版本，只把外语翻译为中文，添加自动翻译 」</p>
+    <img src="https://views.whatilearened.today/views/github/505208/hmjz100.svg" alt="Views">
+    <img src="https://img.shields.io/github/size/ChinaGodMan/UserScripts/translate-only-chinese.user.js?color=%23990000">
+    <p>Download：<a href="https://github.com/ChinaGodMan/UserScripts/tree/main/Script details/translate-only-chinese">Github</a> | ⭐<a
+            href="https://greasyfork.org/zh-CN/scripts/505208">Greasy
+            Fork</a></p> 
+    <img src="https://raw.gitmirror.com/ChinaGodMan/UserScriptsHistory/main/stats/505208.png">
+</div></center>
--- pythonscr/merge-change-history/main.py
+++ pythonscr/merge-change-history/main.py
@@ -1,28 +1,21 @@
 import os
 import json
-
 import markdown  
 import sys
 sys.dont_write_bytecode = True
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from writer import process_markdown
+from writer import process_file
+from content_snippet import get_file_description
 json_file_path = 'docs/ScriptsPath.json'
-
-
 def md_to_html(md_file):
-    
     if not os.path.isfile(md_file):
         print(f"文件 {md_file} 不存在。")
         return None
-    
     with open(md_file, 'r', encoding='utf-8') as f:
         md_text = f.read()
-    
     html_text = markdown.markdown(md_text, extensions=['tables'])
-    
     return html_text
-
-
 # 读取 JSON 文件
 with open(json_file_path, 'r', encoding='utf-8') as json_file:
     data = json.load(json_file)
@@ -33,20 +26,16 @@ for script in data['scripts']:
     description = script.get('description', '')
     greasyfork_id = script.get('GreasyFork', '')
     filepath = script.get('path', '')
-    
     readme_path = os.path.join(backuppath, "Change history", "README.md")
     readme_html = ''
     if os.path.isfile(readme_path):
         readme_html = "<details><summary>更新记录</summary>" + \
             md_to_html(readme_path)+"</details>"
-    
     img_path = os.path.join(backuppath, "preview", "statshistory.png")
     img_tag = ''
     isAddImage = True  # 添加历史安装图片
     if isAddImage:  
-        
         img_tag = f'<img src="https://raw.gitmirror.com/ChinaGodMan/UserScriptsHistory/main/stats/{greasyfork_id}.png">'
-
     html_content = f"""
 <center><div align="center">
     <h1>{name}</h1>
@@ -61,9 +50,15 @@ for script in data['scripts']:
 """
     # 检查 backuppath 是否存在
     if backuppath and os.path.exists(backuppath):
+        start_tag = "<!--AUTO_HISTORY_PLEASE_DONT_DELETE_IT-->"
+        end_tag = "<!--AUTO_HISTORY_PLEASE_DONT_DELETE_IT-END-->"
+        backup_readme_path = os.path.join(backuppath, 'Change history/README.md')
+        his= get_file_description(os.path.join(backuppath, "README.md"), start_tag, end_tag)
+        if "\n"+his+"\n" == html_content:
+                print(f"----[{script.get('name', '')}]\033[91m 更新日志没有改变,拒绝写入!\033[0m")
+                continue
         for file_name in os.listdir(backuppath):
             if file_name.lower().endswith('.md'):
                 file_path = os.path.join(backuppath, file_name)
-                start_tag = "<!--AUTO_HISTORY_PLEASE_DONT_DELETE_IT-->"
-                end_tag = "<!--AUTO_HISTORY_PLEASE_DONT_DELETE_IT-END-->"
-                process_markdown(html_content,file_path,start_tag,end_tag, 'tail' ,False,os.path.join(backuppath, 'Change history/README.md'),True)
+                #process_markdown(html_content, file_path, start_tag, end_tag, 'tail', False, backup_readme_path, True)
+                process_file(file_path, html_content, start_tag, end_tag, "head")
