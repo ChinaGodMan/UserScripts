5e5760f88bc6fd1239498670ea3181be13ffa7eb
人民的勤务员 <96548841+ChinaGodMan@users.noreply.github.com>
2024-10-11 04:35:59 +0000

    更换删除按钮图标为\

--- github-repo-size-view.user.js
+++ github-repo-size-view.user.js
@@ -978,9 +978,7 @@ function insertActiveForks(owner, repo, usePageHeadActions) {
 }
 
 function insertDelBtn(owner, repo, usePageHeadActions, cusClass = 'dialog-show-repo-delete-home', element) {
-    const svgStr = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-  <path d="M2 4h12v2H2V4zm1.33 3h9.34v10a1 1 0 0 1-1 1H4.33a1 1 0 0 1-1-1V7zm1-4h6.34v1H4.33V3zM6 8h1v6H6V8zm3 0h1v6H9V8z"/>
-</svg>`
+    const svgStr = `<div style="display: flex; justify-content: center; align-items: center;"><svg class="octicon octicon-trash color-fg-danger" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path d="M11 1.75V3h2.25a.75.75 0 0 1 0 1.5H2.75a.75.75 0 0 1 0-1.5H5V1.75C5 .784 5.784 0 6.75 0h2.5C10.216 0 11 .784 11 1.75ZM4.496 6.675l.66 6.6a.25.25 0 0 0 .249.225h5.19a.25.25 0 0 0 .249-.225l.66-6.6a.75.75 0 0 1 1.492.149l-.66 6.6A1.748 1.748 0 0 1 10.595 15h-5.19a1.75 1.75 0 0 1-1.741-1.575l-.66-6.6a.75.75 0 1 1 1.492-.15ZM6.5 1.75V3h3V1.75a.25.25 0 0 0-.25-.25h-2.5a.25.25 0 0 0-.25.25Z"></path></svg>`
     const targetUrl = `https://ossinsight.io/analyze/${owner}/${repo}`
     const title = `[${repo}]\n  ${translate.deleteRepo}`
     if (element) {
