9737a5db9c41ddf4dae960d324c0ef41a1a4f279
人民的勤务员 <china.qinwuyuan@gmail.com>
2024-10-18 15:33:08 +0800

    ci: :trollface: `eslint-on-push-pr.yml` (#32)
    thanks
    
    Co-authored-by: adamlui <adam@kudoai.com>

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import json from 'eslint-plugin-json';
+
+export default [
+    js.configs.recommended,
+    {
+        rules: {
+            'indent': 'off', 'no-unexpected-multiline': 'off', 'key-spacing': 'off', // allow whitespace anywhere
+            'quotes': ['error', 'single'], // enforce single quotes for string literals
+            'comma-dangle': ['error', 'never'], // enforce no trailing commas in arrays or objects
+            'no-async-promise-executor': 'off', // allow promise executor functions to be async (to accomodate await lines)
+            'no-constant-condition': 'off', // allow constant conditions
+            'no-empty': 'off', // allow empty blocks
+            'no-inner-declarations': 'off', // allow function declarations anywhere
+            'no-useless-escape': 'off', // allow all escape chars cause ESLint sucks at detecting truly useless ones
+            'no-unused-vars': ['error', { 'caughtErrors': 'none' }] // allow unused named args in catch blocks
+        },
+        languageOptions: {
+            ecmaVersion: 'latest', sourceType: 'script',
+            globals: { ...globals.browser, ...globals.greasemonkey, chatgpt: 'readonly' }
+        }
+    },
+    { files: ['**/*.mjs'], languageOptions: { sourceType: 'module' }},
+    { files: ['**/*.json'], ...json.configs['recommended'] }
+];
