b5dbbe28bf0a5d959d6095c002f939ff8723d2b3
人民的勤务员 <china.qinwuyuan@gmail.com>
2024-10-21 23:50:54 +0800

    docs:  :octocat:  使用`Prettier `格式化 * [yml]和[json]文件

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,8 +6,10 @@ export default [
     js.configs.recommended,
     {
         rules: {
-            'indent': 'off', 'no-unexpected-multiline': 'off', 'key-spacing': 'off', // allow whitespace anywhere
-            'quotes': ['error', 'single'], // enforce single quotes for string literals
+            indent: 'off',
+            'no-unexpected-multiline': 'off',
+            'key-spacing': 'off', // allow whitespace anywhere
+            quotes: ['error', 'single'], // enforce single quotes for string literals
             'comma-dangle': ['error', 'never'], // enforce no trailing commas in arrays or objects
             'no-async-promise-executor': 'off', // allow promise executor functions to be async (to accomodate await lines)
             'no-constant-condition': 'off', // allow constant conditions
@@ -15,12 +17,13 @@ export default [
             'no-inner-declarations': 'off', // allow function declarations anywhere
             'no-useless-escape': 'off', // allow all escape chars cause ESLint sucks at detecting truly useless ones
             //  'no-unused-vars': ['error', { 'caughtErrors': 'none' }],// allow unused named args in catch blocks
-            'no-unused-vars': 'off',// 禁用未使用变量的检查
-            'no-undef': 'off',//禁用未定义变量的检查
+            'no-unused-vars': 'off', // 禁用未使用变量的检查
+            'no-undef': 'off', //禁用未定义变量的检查
             'no-irregular-whitespace': 'error' // 启用不规则空白字符规则
         },
         languageOptions: {
-            ecmaVersion: 'latest', sourceType: 'script',
+            ecmaVersion: 'latest',
+            sourceType: 'script',
             globals: {
                 ...globals.browser,
                 ...globals.greasemonkey,
