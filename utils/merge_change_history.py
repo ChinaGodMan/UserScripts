12f659d5dff750e2ae98da5358a8daecd33fd197
人民的勤务员 <china.qinwuyuan@gmail.com>
2024-10-22 11:25:57 +0800

    chore:  格式化`python`代码 :basecamp: (#45)
    * 使用[autopep8]进行代码格式化

--- utils/merge_change_history.py
+++ utils/merge_change_history.py
@@ -1,12 +1,13 @@
+from content_snippet import get_file_description
+from writer import process_file
 import os
 import json
-import markdown  
+import markdown
 import sys
 sys.dont_write_bytecode = True
-from writer import process_markdown
-from writer import process_file
-from content_snippet import get_file_description
 json_file_path = 'docs/ScriptsPath.json'
+
+
 def md_to_html(md_file):
     if not os.path.isfile(md_file):
         print(f"文件 {md_file} 不存在。")
@@ -15,6 +16,8 @@ def md_to_html(md_file):
         md_text = f.read()
     html_text = markdown.markdown(md_text, extensions=['tables'])
     return html_text
+
+
 # 读取 JSON 文件
 with open(json_file_path, 'r', encoding='utf-8') as json_file:
     data = json.load(json_file)
@@ -24,16 +27,16 @@ for script in data['scripts']:
     name = script.get('name', '')
     description = script.get('description', '')
     greasyfork_id = script.get('GreasyFork', '')
-    filepath =script.get('backuppath', '')+"/"+ script.get('path', '')
+    filepath = script.get('backuppath', '') + "/" + script.get('path', '')
     readme_path = os.path.join(backuppath, "Change history", "README.md")
     readme_html = ''
     if os.path.isfile(readme_path):
         readme_html = "<details><summary>更新记录</summary>" + \
-            md_to_html(readme_path)+"</details>"
+            md_to_html(readme_path) + "</details>"
     img_path = os.path.join(backuppath, "preview", "statshistory.png")
     img_tag = ''
     isAddImage = True  # 添加历史安装图片
-    if isAddImage:  
+    if isAddImage:
         img_tag = f'<img src="https://raw.gitmirror.com/ChinaGodMan/UserScriptsHistory/main/stats/{greasyfork_id}.png">'
     html_content = f"""
 <center><div align="center">
@@ -43,7 +46,7 @@ for script in data['scripts']:
     <img src="https://img.shields.io/github/size/ChinaGodMan/UserScripts/{filepath}?color=%23990000">
     <p>Download：<a href="https://github.com/ChinaGodMan/UserScripts/tree/main/{backuppath}">Github</a> | ⭐<a
             href="https://greasyfork.org/zh-CN/scripts/{greasyfork_id}">Greasy
-            Fork</a></p>{readme_html} 
+            Fork</a></p>{readme_html}
     {img_tag}
 </div></center>
 """
@@ -52,14 +55,12 @@ for script in data['scripts']:
         start_tag = "<!--AUTO_HISTORY_PLEASE_DONT_DELETE_IT-->"
         end_tag = "<!--AUTO_HISTORY_PLEASE_DONT_DELETE_IT-END-->"
         backup_readme_path = os.path.join(backuppath, 'Change history/README.md')
-        his= get_file_description(os.path.join(backuppath, "README.md"), start_tag, end_tag)
-        if "\n"+his+"\n" == html_content:
-                #print(f"----[{script.get('name', '')}]\033[91m 更新日志没有改变,拒绝写入!\033[0m")
-                continue
+        his = get_file_description(os.path.join(backuppath, "README.md"), start_tag, end_tag)
+        if "\n" + his + "\n" == html_content:
+            continue
         else:
             print(f"----[\033[94m{script.get('name', '')}\033[0m]\033[92m 头部描述改变,执行替换!\033[0m")
         for file_name in os.listdir(backuppath):
             if file_name.lower().endswith('.md'):
                 file_path = os.path.join(backuppath, file_name)
-                #process_markdown(html_content, file_path, start_tag, end_tag, 'tail', False, backup_readme_path, True)
                 process_file(file_path, html_content, start_tag, end_tag, "head")
--- utils/upadate_greasyfork.py
+++ utils/upadate_greasyfork.py
@@ -1,35 +1,38 @@
 import requests
 import json
 from datetime import datetime, timedelta
+import sys
 
-# Function to convert UTC to Beijing Time (UTC+8)
+
+# 北京时间
 def convert_to_beijing_time(utc_time):
     utc_dt = datetime.strptime(utc_time, "%Y-%m-%dT%H:%M:%S.%fZ")
     beijing_dt = utc_dt + timedelta(hours=8)
     return beijing_dt.strftime("%Y-%m-%d %H:%M:%S")
 
-# Fetch data from the GreasyFork JSON URL
+
+# 下载数据
 url = "https://greasyfork.org/zh-CN/users/1169082-%E4%BA%BA%E6%B0%91%E7%9A%84%E5%8B%A4%E5%8A%A1%E5%91%98.json"
 response = requests.get(url)
 
 if response.status_code == 200:
-    greasyfork_data = response.json()  # Get the GreasyFork data
+    greasyfork_data = response.json()  # 获取JSON数据
 else:
     print(f"Failed to retrieve data. Status code: {response.status_code}")
-    exit()
+    sys.exit()
 
-# Load your local JSON data (C:\Github\Local Dev\ScriptsPath.json)
+
+# 更新数据
 with open('docs/ScriptsPath.json', "r+", encoding="utf-8") as f:
     local_data = json.load(f)
 
-    # Iterate through each script in the GreasyFork data
+    # 更新脚本信息
     for script in greasyfork_data["scripts"]:
         script_id = str(script["id"])
         created_at_beijing = convert_to_beijing_time(script["created_at"])
         code_updated_at_beijing = convert_to_beijing_time(script["code_updated_at"])
         version = script["version"]
 
-        # Find matching script by GreasyFork id and add the created_at, code_updated_at, and version fields
         for local_script in local_data["scripts"]:
             if local_script.get("GreasyFork") == script_id:
                 local_script["created_at"] = created_at_beijing
