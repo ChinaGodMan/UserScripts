1f5fd27c136bad39b046f487d665c467628825bd
人民的勤务员 <china.qinwuyuan@gmail.com>
2025-03-16 05:35:10 +0800

    feat(userjs): ✨ 下载的文件是`user.js`用户脚本,直接安装.不进行下载

--- github-gist-copier/github-gist-copier.user.js
+++ github-gist-copier/github-gist-copier.user.js
@@ -99,7 +99,7 @@
 // @compatible        qq
 // @compatible        via
 // @compatible        brave
-// @version           2025.03.12.0022
+// @version           2025.03.16.0530
 // @created           2025-03-12 00:22:40
 // @modified          2025-03-12 00:22:40
 // @downloadURL       https://raw.githubusercontent.com/ChinaGodMan/UserScripts/main/github-gist-copier/github-gist-copier.user.js
@@ -111,7 +111,7 @@
  * File Created: 2025/03/12,Wednesday 00:22:50
  * Author: 人民的勤务员@ChinaGodMan (china.qinwuyuan@gmail.com)
  * -----
- * Last Modified: 2025/03/12,Wednesday 02:32:41
+ * Last Modified: 2025/03/16,Sunday 05:32:26
  * Modified By: 人民的勤务员@ChinaGodMan (china.qinwuyuan@gmail.com)
  * -----
  * License: MIT License
@@ -161,7 +161,7 @@
         `
 
         button.innerHTML = `
-        <svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-down">
+        <svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-copy">
             <path d="M0 6.75C0 5.784.784 5 1.75 5h1.5a.75.75 0 0 1 0 1.5h-1.5a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-1.5a.75.75 0 0 1 1.5 0v1.5A1.75 1.75 0 0 1 9.25 16h-7.5A1.75 1.75 0 0 1 0 14.25Z"></path><path d="M5 1.75C5 .784 5.784 0 6.75 0h7.5C15.216 0 16 .784 16 1.75v7.5A1.75 1.75 0 0 1 14.25 11h-7.5A1.75 1.75 0 0 1 5 9.25Zm1.75-.25a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-7.5a.25.25 0 0 0-.25-.25Z"></path>
         </svg>
 
@@ -211,6 +211,12 @@
             e.preventDefault()
             const gistName = element.parentElement.querySelector('.gist-blob-name').innerText
             const rawUrl = rawButton.href
+            if (gistName.slice(-8) === '.user.js') {
+                const a = document.createElement('a')
+                a.href = rawUrl
+                a.click()
+                return
+            }
             GM_xmlhttpRequest({
                 method: 'GET',
                 url: rawUrl,
@@ -292,6 +298,5 @@
     }
 
     runGist()
-
 
 })()
