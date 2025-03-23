21ca0ecae6e44fb3785659ca0decf70bad1b5bd4
人民的勤务员 <china.qinwuyuan@gmail.com>
2025-03-24 04:44:27 +0800

    feat(ci): ✨ 重新启用格式化工作流 * 如果脚本ID为空，此次脚本是首次导入 * 首次导入不格式化，将被[utils/script-import-sync.py]格式化

--- utils/format-changed-scripts.py
+++ utils/format-changed-scripts.py
@@ -11,9 +11,12 @@ for file in changed_files:
         file_path = os.path.join(os.getcwd(), file)
         if os.path.exists(file_path):
             file_name = os.path.basename(file)
-            found = any(script['path'] == file_name for script in scripts_data['scripts'])
-            if not found:
-                print(f'文件 {file_name} 在 JSON 中找不到，跳过操作。')
+            matching_script = next((script for script in scripts_data['scripts'] if script['path'] == file_name), None)
+            if not matching_script:
+                print(f'文件 {file_name}不在列表中,无需格式化')
+                continue
+            if not matching_script.get('GreasyFork') or not matching_script['GreasyFork'].strip():
+                print(f'脚本未发布,此次格式化由\033[38;2;54;114;115m[utils/script-import-sync.py]\033[0m==>\033[38;2;237;147;149m[{file_path}]\033[0m 进行操作.')
                 continue
             target_path = os.path.join(target_dir, file_name)
             subprocess.run(['node', 'utils/single-format-monkey-meta.js', file_path], check=True)
