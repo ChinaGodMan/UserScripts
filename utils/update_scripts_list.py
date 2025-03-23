db47135b1578e17f1784644871c75bed4df6e0b0
人民的勤务员 <china.qinwuyuan@gmail.com>
2025-03-23 08:26:26 +0800

    feat(脚本列表): ✨ :trollface: 添加`OrangeMonkey`脚本管理器图标，来自 adamlui/ai-web-extensions@65fb0a9bd350f6c69e107d89d800d3256bb031b0
    Co-authored-by: adamlui <adam@kudoai.com>

--- utils/update_scripts_list.py
+++ utils/update_scripts_list.py
@@ -37,34 +37,34 @@ def generate_description(all_scripts, single_group=False):
 def generate_html_table(scripts):
     html_table = ''''''
     for script in scripts:
-        full_path = script.get("backuppath") + "/" + script.get("path")
-
         placeholder_image = "https://github.com/ChinaGodMan/UserScriptsHistory/raw/main/20241026_03380458.png"
         img_tag = f'<img width=511 src="{script.get("img")}">' if script.get("img") else f'<img width=511 height=300 src="{placeholder_image}">'
+        script_id = script.get("GreasyFork")
+        script_fold = script.get("backuppath")
         html_table += f'''<h3>
-    <a href="https://github.com/ChinaGodMan/UserScripts/tree/main/{script.get("backuppath")}">
+    <a href="https://github.com/ChinaGodMan/UserScripts/tree/main/{script_fold}">
         <picture><source type="image/png" media="(prefers-color-scheme: dark)" srcset="{script.get("icon")}"><img width=18 src="{script.get("icon")}" width=18></a>
-    <a href="https://github.com/ChinaGodMan/UserScripts/tree/main/{script.get("backuppath")}">{script.get("name")}</a>&nbsp;
-    <a href="https://github.com/ChinaGodMan/UserScripts/tree/main/{script.get("backuppath")}">
-        <img height=24 src="https://img.shields.io/greasyfork/dt/{script.get("GreasyFork")}?logo=greasyfork&logoColor=white&labelColor=%23670000&color=%23670000&style=for-the-badge&label=%E7%94%A8%E6%88%B7%E6%95%B0%E9%87%8F"></a>
+    <a href="https://github.com/ChinaGodMan/UserScripts/tree/main/{script_fold}">{script.get("name")}</a>&nbsp;
+    <a href="https://github.com/ChinaGodMan/UserScripts/tree/main/{script_fold}">
+        <img height=24 src="https://img.shields.io/greasyfork/dt/{script_id}?logo=greasyfork&logoColor=white&labelColor=%23670000&color=%23670000&style=for-the-badge&label=%E7%94%A8%E6%88%B7%E6%95%B0%E9%87%8F"></a>
 </h3>
 
 <details>
     <summary>{script.get("description")}</summary>
     <br><blockquote>
-        <a href="https://github.com/ChinaGodMan/UserScripts/tree/main/{script.get("backuppath")}">
+        <a href="https://github.com/ChinaGodMan/UserScripts/tree/main/{script_fold}">
            {img_tag}</a>
     </blockquote>
 </details>
 
 <blockquote>
-    <a href="https://greasyfork.org/scripts/{script.get("GreasyFork")}">
-        <img height=13 src="https://amazongpt.kudoai.com/assets/images/icons/platforms/tampermonkey/icon28.png"><img height=13.5 src="https://amazongpt.kudoai.com/assets/images/icons/platforms/violentmonkey/icon25.png"></a>
-    <a href="https://greasyfork.org/scripts/{script.get("GreasyFork")}">
+    <a href="https://greasyfork.org/scripts/{script_id}">
+        <img height=13 src="https://amazongpt.kudoai.com/assets/images/icons/platforms/tampermonkey/icon28.png"><img height=13.5 src="https://amazongpt.kudoai.com/assets/images/icons/platforms/violentmonkey/icon25.png"><img height=13 src="https://assets.aiwebextensions.com/images/icons/platforms/scriptcat/icon16.png"><img height=13 src="https://assets.aiwebextensions.com/images/icons/platforms/orangemonkey/icon16.png"></a>
+    <a href="https://greasyfork.org/scripts/{script_id}">
         Greasemonkey</a> /
-    <a href="https://github.com/ChinaGodMan/UserScripts/tree/main/{script.get("backuppath")}/#readme">
+    <a href="https://github.com/ChinaGodMan/UserScripts/tree/main/{script_fold}/#readme">
         <picture><source type="image/svg+xml" media="(prefers-color-scheme: dark)" srcset="https://assets.aiwebextensions.com/images/icons/paper-sheet/white.svg"><img height=13 src="https://assets.aiwebextensions.com/images/icons/paper-sheet/black.svg"></picture></a>
-    <a href="https://github.com/ChinaGodMan/UserScripts/tree/main/{script.get("backuppath")}/#readme">
+    <a href="https://github.com/ChinaGodMan/UserScripts/tree/main/{script_fold}/#readme">
         自述文件</a> /
     <a href="https://github.com/ChinaGodMan/UserScripts/discussions">
         <picture><source type="image/svg+xml" media="(prefers-color-scheme: dark)" srcset="https://assets.aiwebextensions.com/images/icons/speech-bubble-square/white.svg"><img height=12 src="https://assets.aiwebextensions.com/images/icons/speech-bubble-square/black.svg"></picture></a>
@@ -89,7 +89,8 @@ def generate_grouped_html(related_scripts_map, use_details=True, center=False):
         if use_details:
             html_output += f'{center_o}<details><summary>{related_id}</summary>'
         else:
-            html_output += f"<h1>{related_id}</h1>"
+            # 不分组时，添加分隔符用于区分
+            html_output += f'<img height=6px width="100%" src="https://media.chatgptautorefresh.com/images/separators/gradient-aqua.png?latest"><h1>{related_id}</h1>'
         html_output += generate_html_table(scripts)
         if use_details:
             print(center_c)
