e06e734b0ca3039446bb9cb6d42848ac23d1044c
人民的勤务员 <china.qinwuyuan@gmail.com>
2025-03-24 16:38:50 +0800

    refactor(并发限制): ♻️ 每次仅允许操作`3`个`readme_[lang].md`文件进行，再大谷歌就限制访问了

--- utils/translate_chinese_to_filelang.py
+++ utils/translate_chinese_to_filelang.py
@@ -1,5 +1,5 @@
 import os
-import threading
+from concurrent.futures import ThreadPoolExecutor
 from google_translate import translate_and_save
 from google_translate import replace_encoded_with_utf8
 from google_translate import extract_chinese_texts
@@ -9,13 +9,9 @@ from helper import read_json
 from helper import extract_lang_code
 
 
-# 翻译并行任务
+# 翻译单个文件
 def process_file(root, file, lang_code):
     file_path = os.path.join(root, file)
-    if is_file_updated_more_than(file_path, 30):
-        print(f"翻译时跳过文件 {file_path}，因为文件在30分钟之内没有更新")
-        return  # 使用 return 结束当前函数的执行，而不是 continue
-    print(f"开始处理文件: {file_path}，语言代码: {lang_code}")
     # 读取文件内容
     lines = read_file_to_memory(file_path)
     # 替换编码内容为中文
@@ -26,34 +22,37 @@ def process_file(root, file, lang_code):
     translate_and_save(lines, chinese_texts, lang_code, False, file_path)
 
 
-# 主函数，遍历二级目录并处理 README_xx.md 文件
-def process_files():
+# 遍历所有的脚本,投递任务进行翻译
+def go_work():
     json_path = 'docs/ScriptsPath.json'
     data = read_json(json_path)
     scripts = data.get('scripts', [])
-    file_threads = []  # 存储处理文件的线程
-    # max_threads = 20  # 设置最大同时处理的线程数
+    MAX_THREADS = 3  # 每次只允许操作三个文件,再大谷歌就限制请求了.
+    FILE_TIMEOUT = 30
+    GREEN = '\033[32m'
+    YELLOW = '\033[33m'
+    RESET = '\033[0m'
+    with ThreadPoolExecutor(max_workers=MAX_THREADS) as executor:
+        for script in scripts:
+            backuppath = script.get('backuppath', '')
+            for root, dirs, files in os.walk(backuppath):
+                dirs.clear()  # 不允许递归遍历.只让在脚本目录
+                TEMP_OUT = False
+                for file in files:
+                    if file == 'README.md':  # 直接退出,无需下面进行提取语言代码再退出.
+                        continue
+                    # 匹配 README_xx.md 格式的文件，并提取语言代码
+                    lang_code = extract_lang_code(file)
+                    if lang_code is None:
+                        continue
+                    if is_file_updated_more_than(f'{root}/{file}', FILE_TIMEOUT):
+                        continue
+                    else:
+                        if (not TEMP_OUT):
+                            TEMP_OUT = True
+                            print(f"正在处理脚本: {GREEN}{script.get('name', '')}{RESET} [{YELLOW}{root}{RESET}]")
+                        print(f"{YELLOW}    ==>处理文件: [{root}/{file}] |语言代码: {lang_code}{RESET}")
+                    executor.submit(process_file, root, file, lang_code)
 
-    for script in scripts:
-        backuppath = script.get('backuppath', '')
-        for root, dirs, files in os.walk(backuppath):
-            for file in files:
-                # 排除 README.md 文件，仅处理 README_xx.md 格式的文件
-                if file == 'README.md':
-                    continue
-                # 匹配 README_xx.md 格式的文件，并提取语言代码
-                lang_code = extract_lang_code(file)
-                if lang_code is None:
-                    continue
-            # while threading.active_count() > max_threads:time.sleep(0.1)
-                # 为每个文件启动一个线程处理
-                thread = threading.Thread(target=process_file, args=(root, file, lang_code))
-                file_threads.append(thread)
-                thread.start()
 
-    # 等待所有文件处理完成
-    for thread in file_threads:
-        thread.join()
-
-
-process_files()
+go_work()
